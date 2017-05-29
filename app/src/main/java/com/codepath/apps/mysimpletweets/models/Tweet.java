b746eac5082c8f0e0a278b68a0275144031dce32
bigmak712 <bigmak712@gmail.com>
2017-05-29 11:55:45 -0700

    Initial push

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/models/Tweet.java
@@ -0,0 +1,69 @@
+package com.codepath.apps.mysimpletweets.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bigmak712 on 5/28/17.
+ */
+
+// Parse the JSON + store the data, encapsulate state logic or display logic
+public class Tweet {
+    // list out the attributes
+    private String body;
+    private long uid; // unique id for the tweet
+    private User user; // store the embedded user object
+    private String createdAt;
+
+    // Deserialize the JSON and build Tweet objects
+
+    public String getBody() { return body; }
+
+    public long getUid() { return uid; }
+
+    public String getCreatedAt() { return createdAt; }
+
+    public User getUser() { return user; }
+
+    public static Tweet fromJSON(JSONObject jsonObject) {
+        Tweet tweet = new Tweet();
+
+        // Extract the values from the json, store them
+        try {
+            tweet.body = jsonObject.getString("text");
+            tweet.uid = jsonObject.getLong("id");
+            tweet.createdAt = jsonObject.getString("created_at");
+            tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        // Return the tweet object
+        return tweet;
+    }
+
+    // Tweet.fromJSONArray({...}) -> List of Tweets
+    public static ArrayList<Tweet> fromJSONArray(JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        // Iterate the json array and create tweets
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject tweetJson = jsonArray.getJSONObject(i);
+                Tweet tweet = Tweet.fromJSON(tweetJson);
+                if(tweet != null) {
+                    tweets.add(tweet);
+                }
+
+            } catch (JSONException e){
+                e.printStackTrace();
+                continue;
+            }
+        }
+        // Return the finished list
+        return tweets;
+    }
+}
